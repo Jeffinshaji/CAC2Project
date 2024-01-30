ff28b69df542986e8110d2ad16c964e262a800b8
GokulManoj000 <gokul.manoj@msds.christuniversity.in>
2024-01-30 23:39:15 +0530

    update

--- ProLearnAssist/user/urls.py
+++ ProLearnAssist/user/urls.py
@@ -4,10 +4,12 @@ from user import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
-    path('login' , views.user_login , name="login"),
+    path('Login' , views.user_login , name="Login"),
     path('courses',views.courses, name="course"),
     path('register' , views.user_register , name="user_register"),
     path('success',views.success,name='success'),
     path('userprofile',views.user_profile,name='userprofile'),
+    path('logout',views.user_logout,name='logout'),
+    path('lists',views.lists,name="lists")
     
 ]
