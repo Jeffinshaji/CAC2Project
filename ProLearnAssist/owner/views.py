c67a8af6c2e66d91d7dc8cfc5e2ed2f065296c63
Jeffin Shaji <jeffinshaji1997@gmail.com>
2024-02-01 14:32:35 +0530

    update

--- ProLearnAssist/owner/views.py
+++ ProLearnAssist/owner/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
+from user.models import User
+
 
 @login_required(login_url='ownerlogin')
 def dashboard(request):
